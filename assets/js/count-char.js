187df3b1e050ed1b638f5271873b469bf6bdde4b
HOMIHOLI <144209766+HOMIHOLI@users.noreply.github.com>
2026-05-06 02:44:35 +0000

    한글 글자수 세기 추가

--- assets/js/count-char.js
+++ assets/js/count-char.js
@@ -9,3 +9,17 @@ for (const ch of text) {
 }
 console.log(`”${text}”`);
 console.log(`'${target}' 글자는 ${count}번 등장합니다.`);
+
+let hangul = 0; //한글이 몇 글자?
+let total = 0; // 전체 텍스트가 몇 글자?
+for (const ch of text) {
+    if (ch !== " ") { // 공백은 빼고 세기
+        total++;
+        if (ch >= "가" && ch <= "힣") {
+            hangul++;
+        }
+    }
+}
+
+const ratio = (hangul / total) * 100;
+console.log(`'한글 글자' 비율: ${ratio.toFixed(1)}%`)
