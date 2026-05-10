96ae3c2a78da7545c0343facf13f0f1506795b56
HOMIHOLI <144209766+HOMIHOLI@users.noreply.github.com>
2026-05-10 15:25:46 +0000

    Q1

--- assets/js/hw10.js
+++ assets/js/hw10.js
@@ -1,1 +1,16 @@
+// [숙제10] JavaScript 기초 연습
+// 2022-17796 이호윤
+
+// Q1 작품 시대를 돌려주는 함수
+function classifyEra(year) {
+  if (year < 1910) {
+    return "개화기 이전";
+  } else if (year < 1945) {
+    return "일제강점기";
+  } else if (year < 1990) {
+    return "해방 이후–현대";
+  } else {
+    return
+  }
+}
 
